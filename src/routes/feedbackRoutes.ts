820dade28bdb910b9cb3d09daaa379b8cdcacea5
ErangaFernando404 <erangafdo1994@gmail.com>
2026-02-24 12:13:22 +0530

    add authenticate part for feedbackroutes

--- src/routes/feedbackRoutes.ts
+++ src/routes/feedbackRoutes.ts
@@ -1,7 +1,10 @@
 import express, { Router } from 'express';
 import { submitFeedback } from '../controller/feedbackController';
+import { authenticate } from '../middleware/auth';
+import { requireRole } from '../middleware/role';
+import { Role } from '../model/authModel';
 
 const router = Router()
 
-router.post('/submit', submitFeedback);
+router.post('/submit', authenticate, requireRole([Role.USER]) , submitFeedback);
 export default router;
